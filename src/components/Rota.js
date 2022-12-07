5228a046efb147b3b304313bbd767a1b0e9d6979
V-Emanuel <gvictor567@gmail.com>
2022-12-07 11:43:28 -0300

    Requisito: Buscar as informações dos filmes pela API fornecida e exibir conforme layout fornecido

--- src/components/Rota.js
+++ src/components/Rota.js
@@ -5,17 +5,20 @@ import { urlMovies } from "../constants/API";
 
 export default function Rota() {
     const [movies, setMovies] = useState([]);
-
+    //
     useEffect(() => {
         const promise = axios.get(urlMovies)
-        promise.then(answer => { setMovies(answer.posterURL.movies) })
+        promise.then(answer => { (setMovies(answer.data)) })
     }, [])
 
     return (
         <>
             <Topo><h1>CINEFLEX</h1></Topo>
             <MoviesList>
-                    {movies.map(item => <img src={item}></img>)}
+                <p>Selecione o filme</p>
+                <div>
+                {movies.map(item => <ContainerMovie><img src={item.posterURL}></img></ContainerMovie>)}
+                </div>
             </MoviesList>
         </>
     );
@@ -40,7 +43,42 @@ const Topo = styled.div`
 
 `;
 const MoviesList = styled.div`
-width: 100vw;
+width: 100%;
 height: 100%;
 display: flex;
+justify-content: center;
+flex-direction: column;
+p{
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 24px;
+    line-height: 28px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    color: #293845;
+    background-color: red;
+}
+div{
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    }
+img{
+    width: 129px;
+}
+`;
+const ContainerMovie = styled.div`
+    width: 145px;
+    height: 209px;
+    background: #FFFFFF;
+    box-shadow: 0px 2px 4px 2px rgba(0, 0, 0, 0.1);
+    border-radius: 3px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 15px 15px 15px 15px;
 `;
